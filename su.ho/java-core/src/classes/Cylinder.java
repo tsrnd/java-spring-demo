b04f3d0ce11754aa7f8e89b1a328d95f6020f755
Su Ho V <su.ho@asiantech.vn>
2018-05-10 11:58:54 +0700

    oop inheritance 🎗

---
+++ su.ho/java-core/src/classes/Cylinder.java
@@ -0,0 +1,27 @@
+package classes;
+
+public class Cylinder extends Circle {
+	private double height;
+
+	public Cylinder() {
+		super();
+		height = 1;
+	}
+
+	public Cylinder(Double height, double radius, String color) {
+		super(radius, color);
+		this.height = height;
+	}
+	
+	public double getHeight() {
+		return height;
+	}
+	
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	public double getVolumn() {
+		return getArea() * height;
+	}
+}
