04cc1ecee4dd91eabf5b78c9049bb2b47c3dba9b
Su Ho V <su.ho@asiantech.vn>
2018-05-10 14:21:31 +0700

    oop interfaces

---
+++ su.ho/java-core/src/classes/interfaces/Movable.java
@@ -0,0 +1,8 @@
+package classes.interfaces;
+
+public interface Movable {
+	public void moveUp();
+	public void moveDown();
+	public void moveLeft();
+	public void moveRight();
+}
---
+++ su.ho/java-core/src/classes/interfaces/MovablePoint.java
@@ -0,0 +1,35 @@
+package classes.interfaces;
+
+public class MovablePoint implements Movable {
+	int x, y;
+	int xSpeed, ySpeed;
+	
+	public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+	}
+
+	@Override
+	public void moveUp() {
+		System.out.println("MoveUp");
+	}
+
+	@Override
+	public void moveDown() {
+		System.out.println("MoveDown");
+	}
+
+	@Override
+	public void moveLeft() {
+		System.out.println("MoveLeft");
+	}
+
+	@Override
+	public void moveRight() {
+		System.out.println("MoveRight");
+	}
+
+}
