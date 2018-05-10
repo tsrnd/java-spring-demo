6033552d3e1a6fab3f969a300459490861a61e77
Su Ho V <su.ho@asiantech.vn>
2018-05-10 11:12:04 +0700

    circle and employee

---
+++ su.ho/java-core/src/classes/Employee.java
@@ -0,0 +1,58 @@
+package classes;
+
+public class Employee {
+	int id;
+	String firstName;
+	String lastName;
+	int salary;
+
+	public Employee(int id, String firstName, String lastName, int salary) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getName() {
+		return firstName + " " + lastName;
+	}
+	
+	public int getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	public int getAnnualSalary() {
+		return salary * 12;
+	}
+	
+	public int raiseSalary(int percent) {
+		return salary * (percent / 100);
+	}
+	
+	public String toString() {
+		return String.format("Employee[id=%d, name=%s, salary=%d]", getId(), getName(), getSalary());
+	}
+	
+	public static void main(String[] args) {
+		Employee employee = new Employee(1, "Su", "Ho", 1000);
+		System.out.println(employee.toString());
+	}
+	
+}
