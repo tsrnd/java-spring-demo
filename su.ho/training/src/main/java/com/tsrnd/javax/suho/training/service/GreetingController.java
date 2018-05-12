4b7040e77b95e3a0a217fa77a1e0a75d2071be2e
Su Ho V <su.ho@asiantech.vn>
2018-05-12 22:45:46 +0700

    first api 🤞

---
+++ su.ho/training/src/main/java/com/tsrnd/javax/suho/training/service/GreetingController.java
@@ -0,0 +1,17 @@
+package com.tsrnd.javax.suho.training.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tsrnd.javax.suho.training.domain.Greeting;
+
+@RestController
+public class GreetingController {
+	private String template = "Hello, %s!";
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		return new Greeting(String.format(template, name));
+	}
+}
