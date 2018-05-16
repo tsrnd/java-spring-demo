ba929fa2a5656536fc14b1a796a08e6b0d26ac9a
Su Ho V <su.ho@asiantech.vn>
2018-05-16 10:58:32 +0700

    utils

---
+++ su.ho/training/src/main/java/com/tsrnd/javax/suho/training/utils/DateUtils.java
@@ -0,0 +1,19 @@
+package com.tsrnd.javax.suho.training.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtils {
+	public static Date toDateFromString(String date) {
+		String pattern =  "yyyy-MM-dd HH:mm:ss";
+		String temp = date;
+		SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        try {
+            return dateFormat.parse(temp);
+        } catch (ParseException e) {
+            System.err.println("Date ParseException, string value:" + date);
+            return null;
+        }
+	}
+}
